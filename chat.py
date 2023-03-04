caffe1d7a77c09a12fc6741191a7ec1414f452dd
Jerry Yang <efjerryyang@outlook.com>
2023-03-04 15:52:36 +0800

    feat: add `!resend` command, add `modified` flag
    主要是重构，并添加了功能 resend，添加了 modified 标记，去除冗余的询问

--- chat.py
+++ chat.py
@@ -100,146 +100,246 @@ def is_command(user_input: str) -> bool:
     return user_input.startswith("!") or user_input in quit_words
 
 
-def save_current_conversation(
-    messages: List[Dict[str, str]],
-    filepath: str,
-    default_prompt: List[Dict[str, str]],
-    enable_prompt: bool = True,
-) -> None:
-    """Save current conversation to a file"""
-    # Ask user to make sure they want to save
-    if enable_prompt:
-        user_message = input("Do you want to save this conversation? [y/n]: ")
-        if user_message.lower() == "n":
-            printmd("**Conversation not saved.**")
+class Conversation:
+    def __init__(self, default_prompt: List[Dict[str, str]]) -> None:
+        self.messages = list(default_prompt)
+        self.default_prompt = list(default_prompt)
+        self.filepath = load_data(self.messages)
+        self.modified = False
+
+    def __len__(self) -> int:
+        return len(self.messages)
+
+    def __getitem__(self, index: int) -> Dict[str, str]:
+        return self.messages[index]
+
+    def __setitem__(self, index: int, value: Dict[str, str]) -> None:
+        self.messages[index] = value
+        self.modified = True
+
+    def __delitem__(self, index: int) -> None:
+        del self.messages[index]
+        self.modified = True
+
+    def __iter__(self):
+        return iter(self.messages)
+
+    def __add_message(self, message: Dict[str, str]) -> None:
+        self.messages.append(message)
+        self.modified = True
+
+    def add_user_message(self, content: str) -> None:
+        self.__add_message({"role": "user", "content": content})
+
+    def add_assistant_message(self, content: str) -> None:
+        self.__add_message({"role": "assistant", "content": content})
+
+    def edit_system_message(self, content: str) -> None:
+        if self.messages[0]["role"] == "system":
+            self.messages[0]["content"] = content
+            self.modified = True
+        else:
+            raise Exception("The first message is not a system message.")
+
+    def save(self, enable_prompt: bool) -> None:
+        if enable_prompt and self.modified:
+            user_input = input("Save conversation? [y/n]: ").strip()
+            if user_input.lower() != "y":
+                printmd("**Conversation not saved.**")
+                return
+
+        if self.modified:
+            if self.filepath:
+                filename = os.path.basename(self.filepath)
+            else:
+                t = datetime.now().strftime("%Y-%m-%d_%H:%M:%S")
+                tmp = f"conversation_{t}.json"
+                filename = input(f"Enter filename to save to [{tmp}]: ").strip()
+                if not filename:
+                    filename = tmp
+            printmd(f"**Conversation save to [{filename}].**")
+            save_data(self.messages, filename)
+        else:
+            printmd("**Conversation not modified. Nothing to save.**")
+
+    def load(self) -> None:
+        if self.modified:
+            user_input = input("Save conversation? [y/n]: ").strip()
+            if user_input.lower() == "y":
+                self.save(enable_prompt=False)
+        self.messages = list(self.default_prompt)
+        self.filepath = load_data(self.messages)
+        self.modified = False
+        printpnl("### Conversation loaded.", "ChatGPT CLI", "green", 120)
+
+    def reset(self) -> None:
+        if self.modified:
+            user_input = input("Save conversation? [y/n]: ").strip()
+            if user_input.lower() == "y":
+                self.save(enable_prompt=False)
+        self.messages = list(self.default_prompt)
+        self.modified = False
+        printpnl("### Conversation reset.", "ChatGPT CLI", "green", 120)
+
+    def resend(self) -> None:
+        self.resend_last_prompt()
+
+    def resend_last_prompt(self) -> None:
+        if len(self.messages) < 2:
+            printmd("**No previous prompt to resend.**")
             return
-    # Check if this conversation is the same as the default_prompt
-    if is_same_message(messages, default_prompt):
-        printmd(
-            "**This conversation is the same as the default prompt. Nothing to save.**"
-        )
-        return
-    # Save conversation
-    if filepath:
-        filename = os.path.basename(filepath)
-        check = input(f"Save to [{filename}]? [y/n]: ")
-        if check.lower() == "n":
-            printmd("**Conversation not saved.**")
+        # Resend last prompt
+        last_message = self.messages[-1]
+        if last_message["role"] == "user":
+            self.messages[-1]["content"] = generate_response(self.messages)
+            self.modified = True
+            printmd("**Last prompt resent.**")
+            assistant_output(self.messages[-1]["content"])
+        else:
+            printmd("**Last message is assistant message. Nothing to resend.**")
+
+    def regen(self) -> None:
+        self.regenerate_last_response()
+
+    def regenerate_last_response(self) -> None:
+        """Regenerate last response"""
+        if len(self.messages) < 2:
+            printmd("**No previous response to regenerate.**")
             return
-        save_data(messages, filename)
-    else:
-        t = datetime.now().strftime("%Y-%m-%d_%H:%M:%S")
-        tmp = f"conversation_{t}.json"
-        filename = input(f"Enter filename to save to [{tmp}]: ").strip()
-        if not filename:
-            filename = tmp
-        save_data(messages, filename)
-    printmd("**Conversation saved.**")
+        # Regenerate last response
+        last_message = self.messages[-1]
+        if last_message["role"] == "user":
+            printmd(
+                "**Last message is user message. Nothing to regenerate. You may want to use `!resend` instead.**"
+            )
+            return
+        self.messages[-1]["content"] = generate_response(self.messages[:-1])
+        self.modified = True
+        assistant_output(self.messages[-1]["content"])
+        printmd("**Last response regenerated.**")
 
-
-def load_existing_conversation(
-    messages: List[Dict[str, str]], default_prompt: List[Dict[str, str]]
-) -> List[Dict[str, str]]:
-    messages.clear()
-    messages.extend(list(default_prompt))
-    return load_data(messages)
-
-
-def start_new_converstation(
-    messages: List[Dict[str, str]], default_prompt: List[Dict[str, str]]
-) -> None:
-    messages.clear()
-    messages.extend(list(default_prompt))
-
-
-def regenerate_last_response(messages: List[Dict[str, str]]):
-    """Regenerate last response"""
-    last_msg = messages[-1]
-    if last_msg["role"] == "user":
-        printmd("**You can't regenerate your own response. You may want to drop it first.**")
-        return
-    # Regenerate last response
-    messages[-1]["content"] = generate_response(messages)
-    assistant_output(messages[-1]["content"])
-    printmd("**Last response regenerated.**")
-
-
-def edit_messages(messages: List[Dict[str, str]]) -> None:
-    for i in range(len(messages)):
-        msg = messages[i]
-        printpnl(
-            f"{i}. {msg['role']}: {msg['content']}\n", "Editing Messages", "yellow"
-        )
-        print("Enter new message (or Enter to skip).")
-        new_msg = user_input()
-        if new_msg:
-            messages[i]["content"] = new_msg
-            printmd("**Message updated.**\n")
+    def __edit_message(self, index: int, prompt=True) -> None:
+        """Edit message"""
+        if prompt:
+            msg = self.messages[index]
+            printpnl(f"### Editing Message {index}", "Editing Messages", "yellow")
+            show_message(msg)
+        new_content = user_input("\nEnter new content, leave blank to skip: ")
+        if new_content:
+            self.messages[index]["content"] = new_content
+            self.modified = True
+            printmd("**Message edited.**")
         else:
-            printmd("**Message not updated.**\n")
-    printpnl("## Done.", "Editing Messages", "white", 120)
+            printmd("**Message not edited.**")
 
-
-def select_from_messages(messages: List[Dict[str, str]]) -> List[int]:
-    index_list = []
-    for i, msg in enumerate(messages):
-        printpnl(
-            f"{i}. {msg['role']}: {msg['content']}\n", "Selecting Messages", "yellow"
-        )
-        index = input("Select this message to drop? [y/n]: ").strip()
-        if index.lower() == "y":
-            index_list.append(i)
-            printmd("**Message selected.**\n")
+    def show_history(self, index=False, panel=True):
+        """Show conversation history"""
+        if panel:
+            printpnl("### Messages History", "ChatGPT CLI", "green")
+        if index:
+            for i in range(len(self.messages)):
+                printpnl(f"### Message {i}", "Messages History", "green")
+                show_message(self.messages[i])
         else:
-            printmd("**Message not selected.**\n")
-    return index_list
+            for message in self.messages:
+                show_message(message)
 
+    def edit_messages(self) -> None:
+        """Edit messages"""
+        if len(self.messages) == 0:
+            printmd("**No message to edit.**")
+            return
+        # Show messages history with index to select, or iterate through them one by one?
+        choose = input(
+            "Show messages history with index to select, or iterate through them one by one? [i/o]: "
+        ).strip()
+        if choose.lower() == "i":
+            # Show messages history with index to select
+            printpnl("### Messages History", "Editing Messages", "yellow")
+            self.show_history(index=True, panel=False)
+            index = (
+                input(
+                    "Enter index of messages to edit, separate with comma (e.g. 0,1,2), leave blank to cancel: "
+                )
+                .strip()
+                .strip(",")
+            )
+            if not index:
+                printmd("**Editing cancelled.**")
+                return
+            try:
+                index = [int(i.strip()) for i in index.split(",")]
+                for i in index:
+                    if i >= len(self.messages):
+                        raise ValueError
+            except ValueError:
+                printmd("**Invalid index. Editing cancelled.**")
+                return
+            for i in index:
+                self.__edit_message(i, prompt=True)
+        else:  # choose.lower() == "o":
+            for i in range(len(self.messages)):
+                self.__edit_message(i, prompt=True)
 
-def drop_selected_messages(messages: list[Dict[str, str]], index_list: List[int]):
-    for i in sorted(index_list, reverse=True):
-        msg = messages[i]
-        printpnl(f"{i}. {msg['role']}: {msg['content']}\n", "Dropping Messages", "yellow")
-        index = input("Drop this message? [y/n]: ").strip()
-        if index.lower() == "y":
-            messages.pop(i)
-            printmd("**Message dropped.**\n")
+    def drop_messages(self) -> None:
+        """Drop messages"""
+        if len(self.messages) == 0:
+            printmd("**No message to drop.**")
+            return
+        index = []
+        for i, msg in enumerate(self.messages):
+            printpnl(f"### Message {i}", "Dropping Messages", "yellow")
+            show_message(msg)
+            user_input = input("Select this message to drop? [y/n]: ").strip()
+            if user_input.lower() == "y":
+                index.append(i)
+                printmd("**Message selected.**")
+            else:
+                printmd("**Message not selected.**")
+        if index:
+            for i in reversed(index):
+                printpnl(f"### Message {i}", "Dropping Messages", "red")
+                show_message(self.messages[i])
+                user_input = input("Drop this message? [y/n]: ").strip()
+                if user_input.lower() == "y":
+                    self.messages.pop(i)
+                    self.modified = True
+                    printmd("**Message dropped.**")
+                else:
+                    printmd("**Message not dropped.**")
+            printmd("**Selected messages dropped.**")
         else:
-            printmd("**Message not dropped.**\n")
-    printpnl("## Done.", "Dropping Messages", "white", 120)
+            printmd("**No message selected. Dropping cancelled.**")
 
 
 def execute_command(
     user_input: str,
-    messages: List[Dict[str, str]],
-    filepath: str,
-    default_prompt: List[Dict[str, str]],
+    conv: Conversation,
 ) -> str:
     user_input = user_input.strip()
-    if user_input == "!help":
+    if user_input in ["!help", "help"]:
         show_welcome_panel()
-    elif user_input == "!show":
-        display_history(messages)
-    elif user_input == "!save":
-        # examine whether the conetent is the same as default_prompt
-        # ask user to check if save current file
-        save_current_conversation(messages, filepath, default_prompt, False)
-    elif user_input == "!load":
-        save_current_conversation(messages, filepath, default_prompt)
-        filepath = load_existing_conversation(messages, default_prompt)
-        display_history(messages)
-    elif user_input == "!new":
-        save_current_conversation(messages, filepath, default_prompt)
-        start_new_converstation(messages, default_prompt)
-        display_history(messages)
-    elif user_input == "!regen":
-        regenerate_last_response(messages)
-    elif user_input == "!edit":
-        edit_messages(messages)
-    elif user_input == "!drop":
-        index_list = select_from_messages(messages)
-        drop_selected_messages(messages, index_list)
+    elif user_input in ["!show", "show"]:
+        conv.show_history()
+    elif user_input in ["!save", "save"]:
+        conv.save(False)
+    elif user_input in ["!load", "load"]:
+        conv.load()
+
+        conv.show_history(panel=False)
+    elif user_input in ["!new", "new", "reset", "!reset"]:
+        conv.reset()
+        conv.show_history(panel=False)
+    elif user_input in ["!resend", "resend"]:
+        conv.resend()
+    elif user_input in ["!regen", "regen"]:
+        conv.regen()
+    elif user_input in ["!edit", "edit"]:
+        conv.edit_messages()
+    elif user_input in ["!drop", "drop"]:
+        conv.drop_messages()
     elif user_input in ["!exit", "!quit", "quit", "exit"]:
-        save_current_conversation(messages, filepath, default_prompt)
+        conv.save(True)
         print("Bye!")
         exit(0)
     elif user_input.startswith("!"):
@@ -277,7 +377,7 @@ def load_data(messages: List[Dict[str, str]]) -> str:
         return ""
 
     # prompt user to select a file to load
-    print("Available data files:")
+    print("Available data files:\n")
     for i, f in enumerate(files):
         print(f"{i+1}. {f}")
     for a in range(3):
@@ -312,12 +412,11 @@ def printmd(msg: str, newline=True) -> None:
         print()
 
 
-def user_input() -> str:
+def user_input(prompt="\nUser: ") -> str:
     """
     Get user input with support for multiple lines without submitting the message.
     """
     # Use readline to handle user input
-    prompt = "\nUser: "
     lines = []
     while True:
         line = input(prompt)
@@ -328,10 +427,21 @@ def user_input() -> str:
         prompt = "\r" + " " * len(prompt) + "\r" + " .... " + readline.get_line_buffer()
     # Print a message indicating that the input has been submitted
     msg = "\n".join(lines)
-    # handle msg in execute_command
-    # user_output(msg + "\n**[Input Submitted]**")
     printmd("**[Input Submitted]**")
     return msg
+
+
+def show_message(msg: Dict[str, str]) -> None:
+    role = msg["role"]
+    content = msg["content"]
+    if role == "user":
+        user_output(content)
+    elif role == "assistant":
+        assistant_output(content)
+    elif role == "system":
+        system_output(content)
+    else:
+        raise ValueError(f"Invalid role: {role}")
 
 
 def user_output(msg: str) -> None:
@@ -343,7 +453,7 @@ def assistant_output(msg: str) -> None:
 
 
 def system_output(msg: str) -> None:
-    printmd("**System Prompt to ChatGPT:** {}".format(msg))
+    printmd("**System:** {}".format(msg))
 
 
 def show_welcome_panel():
@@ -360,8 +470,9 @@ Here are some useful commands you may want to use:
 - `!show`: show current conversation messages
 - `!save`: save current conversation to a `JSON` file
 - `!load`: load a conversation from a `JSON` file
-- `!new`: start a new conversation
+- `!new` or `!reset`: start a new conversation
 - `!regen`: regenerate the last response
+- `!resend`: resend your last prompt to generate response
 - `!edit`: select a message to edit
 - `!drop`: select a message to drop
 - `!exit` or `!quit`: exit the program
@@ -373,36 +484,6 @@ For more detailed documentation, please visit <link_to_wiki> or <link_to_docs>
 Enjoy your chat!
 """
     printpnl(welcome_msg, title="Welcome")
-
-
-def display_history(messages: List[Dict[str, str]]) -> None:
-    print()
-    for msg in messages:
-        if msg["role"] == "user":
-            # printmd("**User:** {}".format(msg["content"]))
-            user_output(msg["content"])
-        elif msg["role"] == "assistant":
-            # console.print(Markdown("**ChatGPT:** {}".format(msg["content"])))
-            assistant_output(msg["content"])
-        else:  # system
-            # console.print(Markdown("**System:** {}".format(msg["content"])))
-            system_output(msg["content"])
-        # newline
-        console.print()
-
-
-def is_same_message(
-    msg_list1: List[Dict[str, str]], msg_list2: List[Dict[str, str]]
-) -> bool:
-    if len(msg_list1) != len(msg_list2):
-        return False
-    for i in range(len(msg_list1)):
-        if (
-            msg_list1[i]["role"] != msg_list2[i]["role"]
-            or msg_list1[i]["content"] != msg_list2[i]["content"]
-        ):
-            return False
-    return True
 
 
 def generate_response(messages: List[Dict[str, str]]) -> str:
@@ -424,30 +505,31 @@ def generate_response(messages: List[Dict[str, str]]) -> str:
             return ""
     except openai.error.InvalidRequestError as invalid_err:
         print(invalid_err)
-        printpnl("**[Invalid Request]**\nPlease revise your messages according to the error message above.")
+        printpnl(
+            "**[Invalid Request]**\nPlease revise your messages according to the error message above."
+        )
         return ""
+
 
 if __name__ == "__main__":
     config = setup_runtime_env()
     default_prompt = config["openai"]["default_prompt"]
     show_welcome_panel()
-    messages = default_prompt.copy()
 
-    filepath = load_data(messages)
-    display_history(messages)
+    conv = Conversation(default_prompt)
+    conv.show_history()
 
     while True:
         user_message = user_input().strip()
         if is_command(user_message):
-            execute_command(user_message, messages, filepath, default_prompt)
+            execute_command(user_message, conv)
             continue
         else:
-            messages.append({"role": "user", "content": user_message})
-
-        assistant_message = generate_response(messages)
+            conv.add_user_message(user_message)
+        assistant_message = generate_response(conv.messages)
         if assistant_message:
             assistant_output(assistant_message)
-            messages.append({"role": "assistant", "content": assistant_message})
+            conv.add_assistant_message(assistant_message)
             continue
         else:
-            save_current_conversation(messages, filepath, default_prompt)
+            conv.save()
